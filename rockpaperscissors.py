6ce63ddb9bfb3addbec40ab9fde44bd0c52cdf65
cem acar <cemacarmail@yahoo.com>
2016-05-01 21:10:36 +0300

    Revert "Kod versiyon düzenleme"
    This reverts commit e9a8eefcb0b0245811bdfb8b08e262d80c9d7a0d.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 playerScore, cpuScore, draw = 0,0,0
 
 def compare(a):
@@ -10,47 +9,47 @@ def compare(a):
         cpu = (randint(0,2))
         
     if(a==cpu):
-        print ("draw")
+        print 'draw'
         draw=draw+1
     elif(cpu == 0 and a == 2):
-        print ("scissors against rock. cpu wins")
+        print 'scissors against rock. cpu wins'
         cpuScore=cpuScore+1
     elif(cpu == 0 and a == 1):
-        print ("paper against rock. " + name + " wins")
+        print 'paper against rock. ' + name + ' wins'
         playerScore=playerScore+1
     elif(cpu == 1 and a == 0):
-        print ("rock against paper. cpu wins")
+        print 'rock against paper. cpu wins'
         cpuScore=cpuScore+1
     elif(cpu == 1 and a == 2):
-        print ("scissors against paper." + name + " wins")
+        print 'scissors against paper. ' + name + ' wins'
         playerScore=playerScore+1
     elif(cpu == 2 and a == 1):
-        print ("paper against scissors. cpu wins")
+        print 'paper against scissors. cpu wins'
         cpuScore=cpuScore+1
     elif(cpu == 2 and a == 0):
-        print ("rock against scissors." + name + " wins")
+        print 'rock against scissors. ' + name + ' wins'
         playerScore=playerScore+1
 
 def process(a):
-    if (a=="r"):
+    if (a=='r'):
         a=0
-    elif(a=="p"):
+    elif(a=='p'):
         a=1
-    elif(a=="s"):
+    elif(a=='s'):
         a=2
     compare(a)
 
 def showStat():
-    print ("{0}: {1} CPU: {2} Draws:{3}").format(name, str(playerScore), str(cpuScore), str(draw))
+    print "{0}: {1} CPU: {2} Draws:{3}".format(name, str(playerScore), str(cpuScore), str(draw))
 
-name=input("welcome to rock-paper-scissors. what is your name ?")
-print("welcome again, " + name + ". you can type q to quit or stat to see the score during the game.")
+name=raw_input("welcome to rock-paper-scissors. what is your name ?")
+print("welcome again, "+ name+". you can type q to quit or stat to see the score during the game.")
         
 while(True):
-    a = str.lower(input("(r)ock, (p)aper or (s)cissors ?"))
-    if(a=="q"):
+    a = str.lower(raw_input("(r)ock, (p)aper or (s)cissors ?"))
+    if(a=='q'):
         quit()
-    if(a=="stat"):
+    if(a=='stat'):
         showStat()
-    if( a=="r" or a=="p" or a=="s"):
+    if( a=='r' or a=='p' or a=='s'):
         process(a)
